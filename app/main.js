43ff429f9881e5d72666fc1d8983aece13d937a3
Clark Murray <clarkamurray@gmail.com>
2017-10-06 14:06:01 -0400

    Can remove items from list

--- app/main.js
+++ app/main.js
@@ -7,11 +7,27 @@ var toDo = new Vue ({
 		tasks: [], 
 	},
 
+	filters: {
+    capitalize: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  },
+
 	methods: {
 
 		addToList: function() {
 			this.tasks.push(this.newTask);
+			console.log(this.newTask);
+			console.log(this.tasks);
+			this.newTask = "";
 
+		},
+
+		removeItem: function(index) {
+			console.log("Entered");
+			this.tasks.splice(index, (index + 1));
 		}
 	}
 
--- docs/requirements.md
+++ docs/requirements.md
@@ -1,2 +1,12 @@
 # Requirements
 
+[x] Have a list of tasks generated from user input
+[ ] Delete unwanted items
+[x] Capitalize the first letter of the string
+[x] Clear input field after each submission
+[ ] Give input field focus after each submission
+[ ] Press Enter to submit
+[ ] Add Bootstrap & Style
+[ ] Have 'X' clickable that deletes the item on the list
+[ ] Highlight by level of importance
+[ ] Drag and drop for order
